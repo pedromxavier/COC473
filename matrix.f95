fe53c1fd2c969b5b3235650909044d7511ad1039
Pedro <pedromxavier@poli.ufrj.br>
2020-03-19 17:13:03 -0300

    :D

--- matrix.f95
+++ matrix.f95
@@ -20,12 +20,76 @@
             end do
 
         end function
-            
-        subroutine LU_DECOMP(A, b, n, L, U)
+
+        subroutine SWAP_ROWS(A, i, j, n)
             implicit none
 
             integer :: n
-            double precision :: A(n, n), b(n), L(n, n), U(n, n)
+            integer :: i, j
+            double precision A(n, n)
+            double precision temp(n)
+
+            temp(:) = A(i, :)
+            A(i, :) = A(j, :)
+            A(j, :) = temp(:)
+
+        end subroutine 
+
+        function ROW_MAX(A, j, n) result(k)
+            implicit none
+
+            integer :: n
+            double precision A(n, n)
+            
+            integer :: i, j, k
+            double precision :: s
+
+            s = 0.0D0
+            do i = j, n
+                if (A(i, j) > s) then
+                    s = A(i, j)
+                    k = i
+                end if
+            end do 
+
+        end function
+
+        function COND(A, n) result (x)
+            implicit none
+            
+            integer :: n
+            double precision :: A(n, n)
+            double precision :: x
+
+
+        end function
+
+        function PIVOT_MATRIX(A, n) result (M)
+            implicit none
+
+            integer :: n
+            double precision :: A(n, n)
+            double precision :: M(n, n)
+
+            integer :: i, j, k
+
+            M = ID_MATRIX(n)
+
+            do j = 1, n
+                k = ROW_MAX(A, j, n)
+                if (j /= k) then 
+                    call SWAP_ROWS(M, i, j, n)
+                end if
+            end do
+
+            return
+        end function
+            
+        subroutine LU_DECOMP(A, L, U, n)
+            implicit none
+
+            integer :: n
+            double precision :: A(n, n), L(n, n), U(n, n)
             double precision :: s
 
             integer :: i, j, k
@@ -109,7 +173,7 @@
             end do
         end subroutine
 
-        recursive function det(A, n) result (D)
+        recursive function det(A, n) result (d)
             implicit none
 
             integer :: n
@@ -117,17 +181,19 @@
             double precision :: X(n-1, n-1)
 
             integer :: i, j, k
-            double precision :: D
+            double precision :: d, s
 
             if (n == 2) then
-                D = A(1, 1) * A(2, 2) - A(1, 2) * A(2, 1)
+                d = A(1, 1) * A(2, 2) - A(1, 2) * A(2, 1)
             else
-                D = 0.0D0
+                d = 0.0D0
+                s = 1.0D0
                 do i = 1, n
                     X(:,  :i-1) = A(2:,    :i-1)
                     X(:, i:   ) = A(2:, i+1:   )
 
-                    D = det(X, n-1) * A(1, i) + D
+                    d = s * det(X, n-1) * A(1, i) + d
+                    s = -s
                 end do
             end if
                 
@@ -144,11 +210,11 @@
 
         integer, parameter :: n = 3
 
-        double precision :: A(n, n), L(n, n), U(n, n), I(n, n)
+!       Matrices
+        double precision :: A(n, n), LU(n, n), L(n, n), U(n, n), I(n, n)
 
-        double precision :: x(n)
-
-        integer :: i, j
+!       Vectors
+        double precision :: b(n)
 
 !       Define matrix
         A(1,1) = 1.0D0
@@ -167,7 +233,7 @@
         write(*, *) 'A:'
 
 !       Print Matrix
-        call print_matrix(A, m, n)
+        call print_matrix(A, n, n)
 
 !       Print its Determinant
 
@@ -176,29 +242,44 @@
         write(*, *) ''
 
 !       Define Identity Matrix
+        I = ID_MATRIX(n)
 
 !       Print Matrix Name
         write(*, *) 'I:'
 
 !       Print Matrix
-        call print_matrix(ID_MATRIX(n), n, n)
+        call print_matrix(I, n, n)
 
 !       Print its Determinant
 
-        write(*, *) 'Matrix Det:', det(ID_MATRIX(n), n)
+        write(*, *) 'Matrix Det:', det(I, n)
 
         write(*, *) ''
 
 !       Define Vector
-        x(1) = 3.0D0
-        x(2) = 1.0D0
-        x(3) = -5.0D0
+        b(1) = 3.0D0
+        b(2) = 1.0D0
+        b(3) = -5.0D0
 
 !       Print Vector
-        call print_vector(x, m)
+        call print_vector(b, n)
 
 !       LU - Decomposition
+        call LU_DECOMP(A, L, U, n)
 
-        
+!       Print matrices
+        write(*, *) 'L:'
+        call print_matrix(L, n, n)
+
+        write(*, *) ''
+
+        write(*, *) 'U:'
+        call print_matrix(U, n, n)
+
+        write(*, *) ''
+
+        write(*, *) 'LU:'
+        LU = matmul(L, U)
+        call print_matrix(LU, n, n)
 
     end program matrix_test
