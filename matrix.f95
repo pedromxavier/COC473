68ce9a84a5a500a80fd7fdebdae184d8021978bc
Pedro <pedromxavier@poli.ufrj.br>
2020-03-22 03:10:41 -0300

    <3

--- matrix.f95
+++ matrix.f95
@@ -5,6 +5,27 @@
 
     contains
 
+        subroutine read_matrix(fname, A, m, n)
+            implicit none
+            character(len=*) :: fname
+            integer :: m, n
+            double precision, allocatable :: A(:, :)
+
+            integer :: i
+
+            open(unit=33, file=fname, status='old', action='read')
+            read(33, *) m
+            read(33, *) n
+            allocate(A(m, n))
+
+            do i = 1, n
+                read(33,*) A(i,:)
+                write(*,*) A(i,:)
+            enddo
+
+            close(33)
+        end subroutine
+
         function ID_MATRIX(n) result (A)
             implicit none
 
@@ -21,6 +42,47 @@
 
         end function
 
+        recursive function POSITIVE_DEFINITE(A, n) result (x)
+!       Checks wether a matrix is positive definite
+!       according to Sylvester's criterion.
+            implicit none
+
+            integer :: n
+            double precision A(n, n)
+
+            logical :: x
+
+            if (n == 1) then
+                x = (A(1, 1) > 0)
+            else
+                x = POSITIVE_DEFINITE(A(:n-1, :n-1), n-1) .AND. (DET(A, n) > 0)
+            end if
+
+            return
+
+        end function
+
+        function SYMMETRICAL(A, n) result (x)
+!           Check if the Matrix is symmetrical
+            integer :: n
+
+            double precision :: A(n, n)
+
+            integer :: i, j
+            logical :: x
+            
+            do i = 1, n
+                do j = 1, i-1
+                    if (A(i, j) /= A(j, i)) then
+                        x = .FALSE.
+                        return
+                    end if
+                end do
+            end do
+            x = .TRUE.
+            return
+        end function
+
         subroutine SWAP_ROWS(A, i, j, n)
             implicit none
 
@@ -33,7 +95,72 @@
             A(i, :) = A(j, :)
             A(j, :) = temp(:)
 
-        end subroutine 
+        end subroutine
+
+        subroutine ILL_COND()
+            implicit none
+
+            print *, ''//achar(27)//'[95m Matriz mal-condicionada '//achar(27)//'[0m.'
+        end subroutine
+
+        function CHOLESKY_COND(A, n) result (x)
+            implicit none
+
+            integer :: n
+            double precision :: A(n, n)
+
+            logical :: x
+
+            x = SYMMETRICAL(A, n) .AND. POSITIVE_DEFINITE(A, n)
+            return
+
+        end function
+
+        function PLU_COND(A, n) result (x)
+            implicit none
+
+            integer :: n
+            double precision A(n, n)
+
+            integer :: i, j
+            double precision :: s
+
+            logical :: x
+
+            do j = 1, n
+                s = 0.0D0
+                do i = 1, j
+                    if (A(i, j) > s) then
+                        s = A(i, j)
+                    end if
+                end do
+            end do
+            
+            x = (s < 0.01D0)
+
+            return
+        end function
+
+        function LU_COND(A, n) result (x)
+            implicit none
+
+            integer :: n
+            double precision A(n, n)
+
+            integer :: i, j
+            logical :: x
+
+            do j = 1, n
+                do i = 1, j
+                    if (A(i, j) < 0.01D0) then
+                        x = .FALSE.
+                        return
+                    end if 
+                end do
+            end do
+            x = .TRUE.
+            return
+        end function
 
         function ROW_MAX(A, j, n) result(k)
             implicit none
@@ -52,84 +179,91 @@
                 end if
             end do 
 
+            return 
         end function
 
-        function COND(A, n) result (x)
-            implicit none
-            
-            integer :: n
-            double precision :: A(n, n)
-            double precision :: x
-
-
-        end function
-
-        function PIVOT_MATRIX(A, n) result (M)
+        function PIVOT_MATRIX(A, n) result (P)
             implicit none
 
             integer :: n
             double precision :: A(n, n)
-            double precision :: M(n, n)
+            double precision :: P(n, n)
 
-            integer :: i, j, k
+            integer :: j, k
 
-            M = ID_MATRIX(n)
+            P = ID_MATRIX(n)
 
             do j = 1, n
                 k = ROW_MAX(A, j, n)
                 if (j /= k) then 
-                    call SWAP_ROWS(M, i, j, n)
+                    call SWAP_ROWS(P, j, k, n)
                 end if
             end do
 
             return
         end function
-            
-        subroutine LU_DECOMP(A, L, U, n)
+
+        function PLU_DECOMP(A, P, n) result (X)
             implicit none
 
             integer :: n
-            double precision :: A(n, n), L(n, n), U(n, n)
-            double precision :: s
+            double precision :: P(n, n), A(n, n), PA(n, n), X(n, n)
+
+            if (.NOT. PLU_COND(A, n)) then
+                call ILL_COND()
+                return
+            end if
+
+!           Permutation Matrix
+            P = PIVOT_MATRIX(A, n)
+
+!           Row-Swapped Matrix
+            PA = matmul(P, A)
+
+            X = LU_DECOMP(PA, n)
+            return
+
+        end function
+            
+        function LU_DECOMP(A, n) result (X)
+            implicit none
+
+            integer :: n
+            double precision :: A(n, n), X(n, n)
 
             integer :: i, j, k
 
-!           Lower-Triangular Matrix
-            L(:, :) = 0.0D0
+!           Results Matrix
+            X(:, :) = A(:, :)
 
-!           Upper-Triangular Matrix
-            U(:, :) = 0.0D0
+            if (.NOT. LU_COND(A, n)) then
+                call ILL_COND()
+                return
+            end if
 
-            do i = 1, n
-!               Upper-Triangular
-                do k = 1, n
-                    s = 0.0D0
-                    do j = 1, i-1
-                        s = s + L(i, j) * U(j, k)
-                    end do
-                    U(i, k) = A(i, k) - s
+            do k = 1, n-1
+                do i = k+1, n
+                    X(i, k) = X(i, k) / X(k, k)
                 end do
-!               Lower-Triangular
-                do k = 1, n
-                    if (i == k) then
-                        L(i, i) = 1.0D0
-                    else
-                        s = 0.0D0
-                        do j = 1, i-1
-                            s = s + L(k, j) * U(j, i)
-                        end do
-                        L(k, i) = (A(k, i) - s) / U(i, i)
-                    end if
+                
+                do j = k+1, n
+                    do i = k+1, n
+                        X(i, j) = X(i, j) - X(i, k) * A(k, j)
+                    end do
                 end do
             end do
 
-        end subroutine
+            return
+
+        end function
 
         subroutine CHOLESKY_DECOMP(A, n)
             implicit none
 
             integer :: n
             double precision :: A(n, n)
+
+            
 
         end subroutine
 
@@ -141,9 +275,9 @@
 
             integer :: i, j
 
-20          format('|', F6.2, ' ')
-21          format(F6.2, '|')
-22          format(F6.2, ' ')
+20          format('|', F10.5, ' ')
+21          format(F10.5, '|')
+22          format(F10.5, ' ')
 
             do i = 1, m
                 do j = 1, n
@@ -158,6 +292,23 @@
             end do
         end subroutine
 
+        subroutine LU_MATRIX(A, L, U, n)
+            implicit none
+
+            integer :: n
+            double precision :: A(n, n), L(n, n), U(n, n)
+
+            integer :: i
+
+            do i = 1, n
+                L(i, i) = 1.0D0
+                if (i >= 1) then
+                    L(i,  :i-1) = A(i,  :i-1)
+                end if
+                U(i, i:) = A(i, i:)
+            end do
+        end subroutine
+
         subroutine print_vector(x, n)
             implicit none
 
@@ -166,21 +317,21 @@
             
             integer :: i
 
-30          format('|', F6.2, '|')
+30          format('|', F10.5, '|')
 
             do i = 1, n
                 write(*, 30) x(i)
             end do
         end subroutine
 
-        recursive function det(A, n) result (d)
+        recursive function DET(A, n) result (d)
             implicit none
 
             integer :: n
             double precision :: A(n, n)
             double precision :: X(n-1, n-1)
 
-            integer :: i, j, k
+            integer :: i
             double precision :: d, s
 
             if (n == 2) then
@@ -192,12 +343,16 @@
                     X(:,  :i-1) = A(2:,    :i-1)
                     X(:, i:   ) = A(2:, i+1:   )
 
-                    d = s * det(X, n-1) * A(1, i) + d
+                    d = s * DET(X, n-1) * A(1, i) + d
                     s = -s
                 end do
             end if
                 
         end function
+
+!       ================== TEST FUNCTIONS BELOW ================
+
+
 
     end module Matrix
 
@@ -208,26 +363,16 @@
 
         implicit none
 
-        integer, parameter :: n = 3
+        integer :: m, n
 
 !       Matrices
-        double precision :: A(n, n), LU(n, n), L(n, n), U(n, n), I(n, n)
+        double precision, allocatable :: A(:, :)
 
 !       Vectors
-        double precision :: b(n)
+!       double precision :: b(n)
 
 !       Define matrix
-        A(1,1) = 1.0D0
-        A(1,2) = 2.0D0
-        A(1,3) = 4.0D0
-
-        A(2,1) = 0.0D0
-        A(2,2) = 1.0D0
-        A(2,3) = 2.0D0
-
-        A(3,1) = 1.0D0
-        A(3,2) = 0.0D0
-        A(3,3) = 1.0D0
+        call read_matrix('matrix.txt', A, m, n)
 
 !       Print Matrix Name
         write(*, *) 'A:'
@@ -237,49 +382,5 @@
 
 !       Print its Determinant
 
-        write(*, *) 'Matrix Det:', det(A, n)
-
-        write(*, *) ''
-
-!       Define Identity Matrix
-        I = ID_MATRIX(n)
-
-!       Print Matrix Name
-        write(*, *) 'I:'
-
-!       Print Matrix
-        call print_matrix(I, n, n)
-
-!       Print its Determinant
-
-        write(*, *) 'Matrix Det:', det(I, n)
-
-        write(*, *) ''
-
-!       Define Vector
-        b(1) = 3.0D0
-        b(2) = 1.0D0
-        b(3) = -5.0D0
-
-!       Print Vector
-        call print_vector(b, n)
-
-!       LU - Decomposition
-        call LU_DECOMP(A, L, U, n)
-
-!       Print matrices
-        write(*, *) 'L:'
-        call print_matrix(L, n, n)
-
-        write(*, *) ''
-
-        write(*, *) 'U:'
-        call print_matrix(U, n, n)
-
-        write(*, *) ''
-
-        write(*, *) 'LU:'
-        LU = matmul(L, U)
-        call print_matrix(LU, n, n)
-
+        write(*, *) 'Matrix Det:', DET(A, n)
     end program matrix_test
